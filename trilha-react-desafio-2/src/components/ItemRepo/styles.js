aa08257399d73dfb3bc47e5a7e2ab202aa56181b
dudaduarte07 <mariaeduardadepauladuarte@gmail.com>
2024-10-15 17:05:14 -0400

    first commit

---
+++ trilha-react-desafio-2/src/components/ItemRepo/styles.js
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+export const ItemContainer = styled.div`
+  width: 80%;
+
+  h3 {
+    font-size: 32px;
+    color: #fafafa;
+  }
+
+  p {
+    font-size: 16px;
+    color: #fafafa60;
+    margin-bottom: 20px;
+  }
+
+  .remover {
+    background-color: transparent;
+    color: #ff0000;
+    margin-top: 20px;
+    border: 0;
+    cursor: pointer;
+  }
+
+  hr {
+    color: #fafafa60;
+    margin: 20px 0;
+  }
+`;
